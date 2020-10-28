51d44eb19657f2b05759e8b5bf6a4ee652e7ae6f
Warren Hansen <whansen1@mac.com>
2020-10-28 10:25:30 -0700

    version 4

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -15,6 +15,19 @@ class ViewController: UIViewController {
         // version 1
         // version 2
         // version 3
+        
+        /*
+         i want:
+         A-B-C-D-E
+         to become
+         A-B-C-D-E-F
+         where F has exactly the same content as C
+         
+         git reset --hard HEAD~3
+         git revert HEAD     # Reverts E
+         git revert HEAD~2   # Reverts D
+         */
+        //  version 4
     }
 
 
